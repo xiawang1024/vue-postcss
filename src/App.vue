7519892c47700aadca1cfe0abad5ab2dde3d3db7
wangxia/work <xiawang1024@icloud.com>
2018-08-08 15:11:41 +0800

    头部

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-	<div id="app">
-		vue-postcss-scss
-		<wxft></wxft>
-	</div>
+  <div id="app">
+    <wxhd></wxhd>
+    <wxft></wxft>
+  </div>
 </template>
 
 <script>
-
+import wxhd from './base/Header/Header'
 import wxft from './base/Footer/Footer'
+
 export default {
   name: 'App',
   components: {
+    wxhd,
     wxft
   }
 }
---
+++ src/base/Header/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="g-header">
+    <div class="g-hd">
+      <a href="http://www.hndt.com" class="link">
+      </a>
+    </div>
+    <div class="g-banner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'g-header',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.g-hd {
+  position: relative;
+  width: 100%;
+  height: 80px;
+  background: url('./icon-logo-wap.png') center center no-repeat;
+  background-size: auto 60px;
+}
+.g-banner {
+  width: 100%;
+  height: 400px;
+  background: url('./icon-banner-wap.png') center center no-repeat;
+  background-size: cover;
+}
+</style>
+
