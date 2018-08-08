e740f7747c711193546d24ebfade29db09c6a35b
wangxia/work <xiawang1024@icloud.com>
2018-08-08 11:37:29 +0800

    feat:vue postcss scss vw first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import 'common/scss/index.scss';
+
+/* 微信自定义分享 */
+import 'weChat/index';
+
+/* fastclick修复移动端点击延迟300ms */
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+	el: '#app',
+	components: { App },
+	template: '<App/>'
+});
