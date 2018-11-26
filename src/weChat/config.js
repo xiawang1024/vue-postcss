ca89aa2a0e7a9247e464ef9b5c430a71cab6c20f
wangxia/work <xiawang1024@icloud.com>
2018-11-26 15:35:35 +0800

    appId

--- src/weChat/config.js
+++ src/weChat/config.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://a.weixin.hndt.com/h5'
-const appId = 'wx9760b6876d5e339f'
+const appId = 'wx5f789dea59c6c2c5'
 const shareLink = `${baseUrl}/signUp/20180918/signIn/index.html`
 const shareTitle = '签到入场'
 const shareDesc = '2018年广播融媒体技术专业委员会年会'
