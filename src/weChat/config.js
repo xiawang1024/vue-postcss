445a7b053ecc8a915ed87cf594dbfc3fe19d57a9
wangxia/work <xiawang1024@icloud.com>
2018-11-26 15:19:40 +0800

    debug

--- src/weChat/config.js
+++ src/weChat/config.js
@@ -1,9 +1,9 @@
-const baseUrl = 'https://a.weixin.hndt.com/h5';
-const appId = 'wx5f789dea59c6c2c5';
-const shareLink = `${baseUrl}/signUp/20180918/signIn/index.html`;
-const shareTitle = '签到入场';
-const shareDesc = '2018年广播融媒体技术专业委员会年会';
+const baseUrl = 'https://a.weixin.hndt.com/h5'
+const appId = 'wx9760b6876d5e339f'
+const shareLink = `${baseUrl}/signUp/20180918/signIn/index.html`
+const shareTitle = '签到入场'
+const shareDesc = '2018年广播融媒体技术专业委员会年会'
 // const shareImg = require('../common/imgs/logo.png');
-const shareImg = `${baseUrl}/signUp/20180918/logo.png`;
+const shareImg = `${baseUrl}/signUp/20180918/logo.png`
 
-export { appId, shareLink, shareTitle, shareDesc, shareImg };
+export { appId, shareLink, shareTitle, shareDesc, shareImg }
