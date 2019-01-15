dc86e958aff736c70b71f0f1c2487214d3444081
wangxia/work <xiawang1024@icloud.com>
2019-01-15 16:29:37 +0800

    优化api，注入骨架屏

--- src/config/weChatShare_conf.js
+++ src/config/weChatShare_conf.js
@@ -1,7 +1,12 @@
-const appId = 'wx5f789dea59c6c2c5';
-const shareLink = 'https://a.weixin.hndt.com/h5/00/index.html';
-const shareTitle = '分享名称';
-const shareDesc = '分享描述';
-const shareImg = '分享图片';
+const baseURL = 'https://a.weixin.hndt.com/h5/dysw'
+/**
+ * 打鱼晒网微信相关配置
+ * appId 都市频道公众号
+ */
+const appId = 'wxbf02a07137a4e2d3'
+const shareLink = `${baseURL}/index.html`
+const shareTitle = '谁是最酷河南篮球少年投票'
+const shareDesc = '谁是最酷河南篮球少年投票'
+const shareImg = `${baseURL}/icon-share.png`
 
-export { appId, shareLink, shareTitle, shareDesc, shareImg };
+export { appId, shareLink, shareTitle, shareDesc, shareImg }
