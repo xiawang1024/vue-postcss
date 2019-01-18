03d8909046b65bd993805419f056398e4f64211e
wangxia/work <xiawang1024@icloud.com>
2019-01-18 11:13:52 +0800

    标题

--- src/config/weChatShare_conf.js
+++ src/config/weChatShare_conf.js
@@ -5,7 +5,7 @@ const baseURL = 'https://a.weixin.hndt.com/h5/hntv2/dysw'
  */
 const appId = 'wxbf02a07137a4e2d3'
 const shareLink = `${baseURL}/index.html`
-const shareTitle = '“梦联赛”河南首届少儿篮球投票'
+const shareTitle = '谁是最酷河南篮球少年？'
 const shareDesc = '为梦想而战！梦联赛·河南首届少儿篮球精英赛由河南广播电视台主办，河南广播电视台都市频道承办。'
 const shareImg = `${baseURL}/icon-share.png`
 
